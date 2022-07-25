54bf3512081f7dbc67394a331ec32b6503cd12d5
Kate Lee <isoeun809@gmail.com>
2022-07-25 09:36:46 -0500

    Initial Commit

---
+++ To Do List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To Do List
+//
+//  Created by 이소은 on 2022/07/25.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
